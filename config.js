067790a1f8c18c75f0c85acb3fe6cecb867f3c26
Betillo Galvan <45644724+betillogalvanfbc@users.noreply.github.com>
2020-12-12 14:50:42 -0800

    update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Format: https://api.github.com/repos/{owner}/{repo}/issues/
 const GITHUB_ISSUES_LINK =
-     "https://api.github.com/repos/b1ggx/x/issues";
+     "https://api.github.com/repos/b1ggx/z/issues";
 
 const PATH_SEGMENTS_TO_SKIP = 0;
 
